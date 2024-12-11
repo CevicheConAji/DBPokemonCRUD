b0b4309662aed50be4090e0d325ed8904c5faff7
CevicheConAji <zavalachirapiero@gmail.com>
2024-12-11 19:54:02 +0100

    Add database connection and query handling functionality
    Introduced ConnectDB and ControllerDB classes to manage database connections and queries. Updated Main to include a test method utilizing these classes for database operations. Added MySQL dependency to pom.xml and configured project encoding settings.

--- src/main/java/org/example/Controller/ControllerDB.java
+++ src/main/java/org/example/Controller/ControllerDB.java
@@ -1,4 +1,51 @@
 package org.example.Controller;
 
+import org.example.DB.ConnectDB;
+
+import java.sql.*;
+
 public class ControllerDB {
+    private final ConnectDB connectDB = new ConnectDB();
+    private Connection connection = connectDB.getConnection();
+
+    public Connection connectDB() {
+        try{
+            connection = DriverManager.getConnection(connectDB.getJdbc(), connectDB.getUser(), connectDB.getPassword());
+            System.out.println("Connected to database");
+        }catch (SQLException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        return connection;
+    }
+    public void closeDB() {
+        try{
+            connection.close();
+            System.out.println("Disconnected from database");
+
+        }catch (SQLException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+    public ResultSet getconsulta(String query){
+        Statement statement;
+        ResultSet resultSet = null;
+        try{
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+        }catch (SQLException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        return resultSet;
+    }
+    public void showPokeTypeSet(ResultSet rs){
+
+        try{
+            while (rs.next()){
+                String pokeType = rs.getString("pokename");
+                System.out.printf("%-10s\n",pokeType);
+            }
+        }catch (SQLException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.Controller.ControllerDB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        test();
     }
+    public static void test() {
+        ControllerDB controllerDB = new ControllerDB();
+        connection(controllerDB);
+        getPokeTypeSet(9,controllerDB);
+        closeDB(controllerDB);
+    }
+    private static void connection(ControllerDB controllerDB) {
+        controllerDB.connectDB();
+    }
+    private static void closeDB(ControllerDB controllerDB) {
+        controllerDB.closeDB();
+    }
+    private static void getPokeTypeSet(int id,ControllerDB controllerDB){
+        ResultSet rs = controllerDB.getconsulta("SELECT * FROM pokemon WHERE IDpoke = 9");
+        controllerDB.showPokeTypeSet(rs);
+    }
+
 }
